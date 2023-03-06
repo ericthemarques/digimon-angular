ce3a68678506eb2f6615487213bba871a080fd95
Eric Marques Gomes <eric.marquesgomesmarques@gmail.com>
2023-03-06 15:01:59 -0300

    Primeiro dia de estudo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DigimonComponent } from './digimon/digimon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DigimonComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/digimon/digimon.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DigimonComponent } from './digimon.component';
+
+describe('DigimonComponent', () => {
+  let component: DigimonComponent;
+  let fixture: ComponentFixture<DigimonComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DigimonComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DigimonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
